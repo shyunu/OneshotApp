9b723407910462bc1cc0251cd9364a8d3d1f9d15
Jiyoon <chic0813@naver.com>
2024-10-28 16:50:25 +0900

    수정완료

--- screens/contract/ContractWriteScreen.js
+++ screens/contract/ContractWriteScreen.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import ContractWriteEditor from './ContractWriteEditor';
 import ContractWriteHeader from './ContractWriteHeader';
 
 function ContractWriteScreen() {
+  const [clientNo, setClientNo] = useState(null);
+  const [productNo, setProductNo] = useState(null);
+  const [selectedStartDate, setSelectedStartDate] = useState('');
+  const [selectedEndDate, setSelectedEndDate] = useState('');
+  const [contractPrice, setContractPrice] = useState('');
+  const [contractItems, setContractItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   return (
     <SafeAreaView style={styles.block}>
-      <ContractWriteHeader />
-      <ContractWriteEditor />
+      <ContractWriteHeader
+        clientNo={clientNo}
+        setClientNo={setClientNo}
+        productNo={productNo}
+        setProductNo={setProductNo}
+        selectedStartDate={selectedStartDate}
+        setSelectedStartDate={setSelectedStartDate}
+        selectedEndDate={selectedEndDate}
+        setSelectedEndDate={setSelectedEndDate}
+        contractPrice={contractPrice}
+        setContractPrice={setContractPrice}
+        contractItems={contractItems}
+        setContractItems={setContractItems}
+        loading={loading}
+        setLoading={setLoading}
+      />
+      <ContractWriteEditor
+        clientNo={clientNo}
+        setClientNo={setClientNo}
+        productNo={productNo}
+        setProductNo={setProductNo}
+        selectedStartDate={selectedStartDate}
+        setSelectedStartDate={setSelectedStartDate}
+        selectedEndDate={selectedEndDate}
+        setSelectedEndDate={setSelectedEndDate}
+        contractPrice={contractPrice}
+        setContractPrice={setContractPrice}
+        contractItems={contractItems}
+        setContractItems={setContractItems}
+        loading={loading}
+        setLoading={setLoading}
+      />
     </SafeAreaView>
   );
 }
